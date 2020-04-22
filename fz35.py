6dcbf721b0ed2d0fe41a28ce0f7ced8476ee005c
cammiboi <camjnaw@shaw.ca>
2020-04-22 09:53:57 -0700

    add read

--- fz35.py
+++ fz35.py
@@ -16,16 +16,39 @@ class FZ35():
     def send_command(self, command: str):
         print("send command: {}".format(command))
         self.ser.write(command.encode('utf-8'))
-        self.read_reply()
+        result = self.check_success()
+        if result:
+            print("command success")
+        else:
+            print("command fail")
+        return result
 
     def format_number(self, number: float, num_dig_before: int, num_dig_after: int):
         fmt = "0{}.{}f".format(
             num_dig_before + num_dig_after + 1, num_dig_after)
         return format(number, fmt)
 
-    def read_reply(self):
+    def get_reply(self):
         reply = self.ser.read_until(b'\r\n')
-        print("reply: {}".format(reply))
+        reply = reply.decode("utf-8")
+        reply = reply.replace('\r\n', "")
+        return reply
+
+    def check_success(self):
+        for i in range(0,5):
+            reply = self.get_reply()
+            if reply == "sucess":
+                return True
+            elif reply == "success":
+                return True
+            elif reply == "fail":
+                return False
+            elif reply == "":
+                return False
+    
+    def get_measurement(self):
+        reply = self.get_reply()
+        return reply
 
     def turn_on(self):
         self.send_command("on")
@@ -47,7 +70,12 @@ if __name__ == '__main__':
     load.set_current(3)
     sleep(3)
     load.set_current(0.06)
+    load.set_current(0.05)
+    load.set_current(0.03)
+    load.set_current(0.0)
+    load.send_command("dhhdhdff")
     sleep(1)
     load.turn_off()
     sleep(1)
     load.turn_on()
+    print(load.get_measurement())
