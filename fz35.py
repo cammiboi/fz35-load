aef21413068ab6ae3f5691b5ba4beb190a46b99b
Cameron Nawosad <cameron@Camerons-MacBook-Pro.local>
2020-04-22 09:16:29 -0700

    add read reply

--- fz35.py
+++ fz35.py
@@ -1,19 +1,31 @@
 from serial import Serial
 from time import sleep
 
+# wait for new bytes to arrive
+# wait until its success for fail
+# return result
+
+
 class FZ35():
     def __init__(self, serial_port: str):
-        self.ser = Serial(serial_port)
-        self.send_command("start") # tell load to send its measurement every second
+        # open serial port for load and set receieve timeout to 1.5 sec
+        self.ser = Serial(port=serial_port, timeout=1.5)
+        # tell load to send its measurement every second
+        self.send_command("start")
 
     def send_command(self, command: str):
         print("send command: {}".format(command))
         self.ser.write(command.encode('utf-8'))
+        self.read_reply()
 
     def format_number(self, number: float, num_dig_before: int, num_dig_after: int):
         fmt = "0{}.{}f".format(
             num_dig_before + num_dig_after + 1, num_dig_after)
         return format(number, fmt)
+
+    def read_reply(self):
+        reply = self.ser.read_until(b'\r\n')
+        print("reply: {}".format(reply))
 
     def turn_on(self):
         self.send_command("on")
@@ -25,6 +37,7 @@ class FZ35():
         num = self.format_number(current, 1, 2)
         cmd = "{}A".format(num)
         self.send_command(cmd)
+
 
 if __name__ == '__main__':
 
