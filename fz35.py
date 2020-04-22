910498369daae9cc097536223891d2cf80681eae
cammiboi <camjnaw@shaw.ca>
2020-04-22 08:41:19 -0700

    test first example

--- fz35.py
+++ fz35.py
@@ -1,13 +1,14 @@
 import serial
-
+from time import sleep
 
 class FZ35():
     def __init__(self, serial_port: str):
         self.ser = serial.Serial(serial_port)
+        self.send_command("start") # tell load to send its measurement every second
 
     def send_command(self, command: str):
         print("send command: {}".format(command))
-        self.ser.write(command)
+        self.ser.write(command.encode('utf-8'))
 
     def format_number(self, number: float, num_dig_before: int, num_dig_after: int):
         fmt = "0{}.{}f".format(
@@ -21,6 +22,18 @@ class FZ35():
         self.send_command("off")
 
     def set_current(self, current: float):
-        num = format_number(current, 1, 2)
+        num = self.format_number(current, 1, 2)
         cmd = "{}A".format(num)
         self.send_command(cmd)
+
+if __name__ == '__main__':
+
+    # example script to control load
+    load = FZ35("COM8")
+    load.set_current(3)
+    sleep(3)
+    load.set_current(0.06)
+    sleep(1)
+    load.turn_off()
+    sleep(1)
+    load.turn_on()
