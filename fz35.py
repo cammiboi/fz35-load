1843f1e2e45dfc2c96c415e5cedffc289a5bf06c
cammiboi <camjnaw@shaw.ca>
2020-04-25 10:20:16 -0700

    fix missing self argument

--- fz35.py
+++ fz35.py
@@ -8,15 +8,17 @@ class FZ35():
         # tell load to send its measurement every second
         self.send_command("start")
 
-    def send_command(self, command: str):
+    def send_command(self, command: str, check_success: bool = True):
         print("send command: {}".format(command))
         self.ser.write(command.encode('utf-8'))
-        result = self.check_success()
-        if result:
-            print("command success")
-        else:
-            print("command fail")
-        return result
+        if check_success:
+            result = self.check_success()
+            if result:
+                print("command success")
+            else:
+                print("command fail")
+            return result
+        return True
 
     def format_number(self, number: float, num_dig_before: int, num_dig_after: int):
         fmt = "0{}.{}f".format(
@@ -73,32 +75,32 @@ class FZ35():
         cmd = "{}A".format(num)
         self.send_command(cmd)
 
-    def set_low_voltage_protection(voltage: float):
+    def set_low_voltage_protection(self, voltage: float):
         num = self.format_number(voltage, 2, 1)
         cmd = "LVP:{}".format(num)
         self.send_command(cmd)
 
-    def set_over_voltage_protection(voltage: float):
+    def set_over_voltage_protection(self, voltage: float):
         num = self.format_number(voltage, 2, 1)
         cmd = "OVP:{}".format(num)
         self.send_command(cmd)
 
-    def set_over_current_protection(current: float):
+    def set_over_current_protection(self, current: float):
         num = self.format_number(current, 1, 2)
         cmd = "OCP:{}".format(num)
         self.send_command(cmd)
 
-    def set_over_power_protection(power: float):
+    def set_over_power_protection(self, power: float):
         num = self.format_number(power, 2, 2)
         cmd = "OPP:{}".format(num)
         self.send_command(cmd)
 
-    def set_max_ah_protection(ah: float):
+    def set_max_ah_protection(self, ah: float):
         num = self.format_number(ah, 1, 3)
         cmd = "OAH:{}".format(num)
         self.send_command(cmd)
 
-    def set_max_time_protection(min: int):
+    def set_max_time_protection(self, min: int):
         hr = int(min / 60)
         min = min - (hr * 60)
         hr = format(hr, "02.0f")
@@ -106,9 +108,11 @@ class FZ35():
         cmd = "OHP:{}:{}".format(hr, min)
         self.send_command(cmd)
 
-    def get_protection_settings():
-        self.send_command("read")
-        reply = self.get_reply
-        if reply.startswith("OVP"):
-            return reply
+    def get_protection_settings(self):
+        self.send_command("read", check_success=False)
+        for n in range(0,3):
+            reply = self.get_reply()
+            if reply.startswith("OVP"):
+                print("protection settings: {}".format(reply))
+                return reply
         return False
